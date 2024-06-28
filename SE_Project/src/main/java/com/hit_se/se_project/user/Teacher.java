78e354975600489a947b0beaacc28d04f511d099
Syoesnoto <93168016+Syoesnoto@users.noreply.github.com>
2024-06-28 22:25:24 +0800

    Delete SE_Project directory

--- SE_Project/src/main/java/com/hit_se/se_project/user/Teacher.java
+++
@@ -1,8 +0,0 @@
-package com.hit_se.se_project.user;
-
-public class Teacher extends User {
-    public Teacher(String name, String password, boolean permission) {
-        super(name, password, permission);
-        type = "teacher";
-    }
-}
